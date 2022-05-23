7c47f2a20319a9c00b902196373c8a02984baaca
bariknishan <barik6286@gmail.com>
2022-05-23 18:35:51 +0600

    env added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const  cors = require('cors');
 require('dotenv').config();
 const port= process.env.PORT || 5000 ;
@@ -10,12 +11,15 @@ const app= express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
+// database start 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rkd0q.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 app.get('/',(req,res) =>{
     res.send('elctric server is running')
 })
+
+
+// port listen
 app.listen( port,()=>{
     console.log('listening to port', port)
 })

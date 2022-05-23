6eba45a4bf80aa13de3a61fb4d8fc9e9a9f7315a
bariknishan <barik6286@gmail.com>
2022-05-23 19:13:55 +0600

    products added

--- index.js
+++ index.js
@@ -12,8 +12,50 @@ app.use(cors());
 app.use(express.json());
 
 // database start 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rkd0q.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// databse function start 1st
+async function run(){
+try{
+ await client.connect()
+ console.log('database connected');
+ const productsCollecetion = client.db('electric_manufacturer').collection('products');
+ 
+
+
+ app.get('/products', async(req,res)=>{
+    const query={} ;
+    const cursor= productsCollecetion.find(query);
+    const products= await cursor.toArray()
+    res.send(products);
+ })
+
+
+
+}
+finally{
+
+}
+}
+run().catch(console.dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/',(req,res) =>{
     res.send('elctric server is running')
 })

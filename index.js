57b6b2f1f3e30c92707e8664ae902e183cb3582d
bariknishan <barik6286@gmail.com>
2022-05-28 01:46:18 +0600

    payment added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -193,6 +193,25 @@ async function run() {
                 return res.status(403).send({ message: 'forbiddden access' })
             }
         })
+
+
+
+      //////// payment booking for order
+
+app.get('/booking/:id',verifyjwt, async (req,res)=>{
+    const id= req.params.id ;
+    const query= {_id:ObjectId(id)};
+    const booking= await bookingCollecetion.findOne(query);
+    res.send(booking);
+})
+
+
+
+
+
+
+
+
 
 
 
